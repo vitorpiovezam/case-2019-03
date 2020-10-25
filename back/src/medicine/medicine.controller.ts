6be9133c408e53feafcbfc52846a466b9e88bc77
Vitor Piovezam <vitorpiovezam@yandex.com>
2020-10-25 13:58:38 -0300

    paginatation endpoint for medicines

--- back/src/medicine/medicine.controller.ts
+++ back/src/medicine/medicine.controller.ts
@@ -2,10 +2,17 @@ import { MedicineRepository } from './medicine.repository';
 import {
   Controller,
   Get,
+  Put,
+  Query,
 } from '@nestjs/common';
 import { Medicine } from './entities/medicine.entity';
 import * as medicamentosRaw from 'src/dados/medicamentos.json';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 
 @Controller('/api/medicine')
 export class MedicineController {
@@ -14,13 +21,21 @@ export class MedicineController {
     private medicineRepository: MedicineRepository
   ) {}
 
-  @Get('/import')
+  @Put('/import')
   async import(): Promise<any> {
     const medicamentos: Medicine[] = (medicamentosRaw as any[]).map(item => new Medicine(item));
     this.medicineRepository.save(medicamentos, { chunk: medicamentos.length / 1000 });
 
     return {
-      body: `Bulking ${medicamentos.length} medicines ↕`
+      body: `Bulking ${medicamentos.length} medicines, the data is on the way!`
     }
+  }
+
+  @Get('')
+  async paginate(
+    @Query()
+    options: IPaginationOptions
+  ): Promise<Pagination<Medicine>> {
+    return paginate<Medicine>(this.medicineRepository, options);
   }
 }
